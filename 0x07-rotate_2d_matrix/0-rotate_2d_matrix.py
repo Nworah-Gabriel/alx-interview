ea5b8d17ac3041b338463bc0a8812a43487a46ee
Nworah-Gabriel <gabrielnworah6@gmail.com>
2023-03-13 17:33:00 +0100

    Reduced the indentation level

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,18 +9,18 @@ def rotate_2d_matrix(matrix):
     if matrix is None:
         return
     for lists in matrix:
-            count = 0
+        count = 0
+        for liss in lists:
+            if type(liss) == list:
+                return
+        if len(holder) == 0:
             for liss in lists:
-                if type(liss) == list:
-                    return
-            if len(holder) == 0:
+                newlist = []
+                newlist.append(liss)
+                holder.append(newlist)
+        elif len(holder) != 0:
+            number = len(holder)
+            if count <= number:
                 for liss in lists:
-                    newlist = []
-                    newlist.append(liss)
-                    holder.append(newlist)
-            elif len(holder) != 0:
-                number = len(holder)
-                if count <= number:
-                    for liss in lists:
-                        holder[count].append(liss)
-                        count += 1
+                    holder[count].append(liss)
+                    count += 1
