f4c07c8b0d2392e878fdb00acfeef56b76d9c399
Nworah-Gabriel <gabrielnworah6@gmail.com>
2023-03-23 17:53:22 +0100

    0x08. Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Make changes"""
+
+def makeChange(coins, total):
+    """
+    make changes
+    """
+
+    if total == 0:
+        return 0
+    
+    holder = []
+    hold = 0
+    count = 0
+    lenght = len(coins)
+    for coin in coins:
+        for i in coins:
+            hold = coin
+            while hold <= total:
+                if hold < total:
+                    hold += i
+                    count += 1
+                if hold == total:
+                    count += 1
+                    holder.append(count)
+                    hold = 0
+                    count = 0
+                    break
+        if len(holder) > 0:
+            return min(holder)
+        else:
+            return -1
+
